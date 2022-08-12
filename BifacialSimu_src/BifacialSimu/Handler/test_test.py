13181f8e138f13f63aa19d67907fd6332649c6d0
faresaoun <104490180+faresaoun@users.noreply.github.com>
2022-08-12 12:18:48 +0200

    Update test_test.py
    Changed test values to match values in default simulation of GUI
    made some adjustments and removed unnecessary values

--- BifacialSimu_src/BifacialSimu/Handler/test_test.py
+++ BifacialSimu_src/BifacialSimu/Handler/test_test.py
@@ -34,72 +34,49 @@ simulationDict={
     'startHour' : (2019, 11, 1, 0),  # Only for hourly simulation, yy, mm, dd, hh
     'endHour' : (2019, 11, 16, 0),  # Only for hourly simulation, yy, mm, dd, hh
     'utcOffset': -7,
-    'tilt' : 10, #tilt of the PV surface [deg]
+    'tilt' : 25, #tilt of the PV surface [deg]
     'singleAxisTracking' : True, # singleAxisTracking or not
     'backTracking' : False, # Solar backtracking is a tracking control program that aims to minimize PV panel-on-panel shading 
     'ElectricalMode_simple': 0, # simple electrical Simulation after PVSyst, use if rear module parameters are missing
     'limitAngle' : 60, # limit Angle for singleAxisTracking
     'hub_height' : 1.3, # Height of the rotation axis of the tracker [m]
     'azimuth' : 180, #azimuth of the PV surface [deg] 90°: East, 135° : South-East, 180°:South
-    'nModsx' : 1, #number of modules in x-axis
+    'nModsx' : 5, #number of modules in x-axis
     'nModsy' : 1, #number of modules in y-axis
     'nRows' : 3, #number of rows
     'sensorsy' : 5, #number of sensors
-    'moduley' : 2 ,#length of modules in y-axis
-    'modulex' : 1, #length of modules in x-axis  
+    'moduley' : 1.98 ,#length of modules in y-axis
+    'modulex' : 0.992, #length of modules in x-axis  
     'fixAlbedo': False, # Option to use the fix albedo
     'hourlyMeasuredAlbedo' : True, # True if measured albedo values in weather file
     'hourlySpectralAlbedo' : True, # Option to calculate a spectral Albedo 
     'variableAlbedo': False, # Option to calculate sun position dependend, variable albedo
-    'albedo' : 0.26, # Measured Albedo average value, if hourly isn't available
+    'albedo' : 0.247, # Measured Albedo average value, if hourly isn't available
     'frontReflect' : 0.03, #front surface reflectivity of PV rows
     'BackReflect' : 0.05, #back surface reflectivity of PV rows
     'longitude' : -105.172, 
     'latitude' : 39.739,
-    'gcr' : 0.35, #ground coverage ratio (module area / land use)
+    'gcr' : 0.45, #ground coverage ratio (module area / land use)
     'module_type' : 'NREL row 2', #Name of Module
     }
 
-# simulationParameter = {
-# 'n_pvrows': simulationDict['nRows'], #number of PV rows
-# 'number_of_segments': simulationDict['sensorsy'], #number of segments for each PVrow
-# 'pvrow_height': simulationDict['clearance_height'], #height of the PV rows, measured at their center [m]
-# 'pvrow_width': simulationDict['moduley'], #width of the PV panel in row, considered 2D plane [m]
-# 'pvmodule_width': simulationDict['modulex'], #length of the PV panel in row, considered 2D plane [m]
-# 'surface_azimuth': simulationDict['azimuth'], #azimuth of the PV surface [deg] 90°= East, 135° = South-East, 180°=South
-# 'surface_tilt': simulationDict['tilt'], #tilt of the PV surface [deg]
-# 'albedo': simulationDict['albedo'], # Measured Albedo average value
-# #'a0': albedo, # Measured Albedo under direct illumination with a solar zenith angle of approx. 60°
-# #'adiff': albedo_diff, # Measured Albedo under 100% diffuse illumination
-# 'C': 0.4, #Solar angle dependency factor    
-# #'index_observed_pvrow': 1, #index of the PV row, whose incident irradiance will be returned
-# 'rho_front_pvrow' : simulationDict['frontReflect'], #front surface reflectivity of PV rows
-# 'rho_back_pvrow' : simulationDict['BackReflect'], #back surface reflectivity of PV rows
-# 'horizon_band_angle' : 6.5, #elevation angle of the sky dome's diffuse horizon band [deg]   
-# 'L_Position': simulationDict['longitude'], #Longitude of measurement position [deg]
-# 'L_Area': -105.1727827, #Longitude of timezone area [deg]
-# 'Latitude_Position': simulationDict['latitude'], #Latitude of measurement position [deg]
-# 'axis_azimuth': 0.0, #Axis Azimuth angle [deg]
-# 'gcr': simulationDict['gcr'], #ground coverage ratio (module area / land use)
-# 'x_min': -100,
-# 'x_max': 100,
-# }
+simulationName = simulationDict['simulationName']
+onlyFrontscan=False
+onlyBackscan=True
+
 
 resultsPath = DataHandler().setDirectories()
 
-simulationName = simulationDict['simulationName']
-# demo = RadianceObj(name = simulationName, path = resultsPath)
 metdata, demo = DataHandler().getWeatherData(simulationDict, resultsPath)
-dataFrame = pd.DataFrame()
-onlyFrontscan=True
-onlyBackscan=True
-# df=pd.Dataframe()
-# df = DataHandler().passEPWtoDF(metdata, simulationDict, resultsPath)
+
 df = BifacialSimu_dataHandler.DataHandler().passEPWtoDF(metdata, simulationDict, resultsPath)
 df_reportRT = pd.DataFrame()
 df_reportVF = pd.DataFrame()
-df_reportVF, df= BifacialSimu_radiationHandler.ViewFactors.simulateViewFactors(simulationDict, demo, metdata,  df, resultsPath, onlyFrontscan = False)
-df_report = Electrical_simulation.build_simulationReport(df_reportVF, df_reportRT, simulationDict, resultsPath)
+df_report = pd.DataFrame()
+dataFrame = pd.DataFrame()
+
+df_reportVF, df= BifacialSimu_radiationHandler.ViewFactors.simulateViewFactors(simulationDict, demo, metdata,  df, resultsPath, onlyFrontscan)
+# df_report = Electrical_simulation.build_simulationReport(df_reportVF, df_reportRT, simulationDict, resultsPath)
 
 # df_reportVF=ViewFactors.simulateViewFactors(simulationDict, demo, metdata, dataFrame, resultsPath, onlyFrontscan)
 # df_reportRT = RayTrace.simulateRayTrace(simulationDict,demo, metdata, resultsPath, dataFrame, onlyBackscan)
@@ -125,18 +102,13 @@ moduleDict={
 		"modulex": 0.992
 }
    
-    #assertEqual(1,2) #1actual value #2expected
     
-    # df_report={}
-    # df=pd.DataFrame()
-    # resultsPath=""
-    
+result_1= Electrical_simulation.simulate_simpleBifacial(moduleDict, simulationDict, df_reportVF, df_reportRT, df_report, df, resultsPath)
 
 class TestStringMethods(unittest.TestCase):
     
     def test_simulate_simpleBifacial(self):
-        result_1= Electrical_simulation.simulate_simpleBifacial(simulationDict, df_reportVF, df_reportRT, df_report, df, resultsPath)
-        self.assertEqual(result_1, 9.457514227805062)
+        self.assertEqual(result_1, 5.599072940263201)
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
