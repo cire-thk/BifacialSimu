155dcc6e49e7ebf210d42f99928af7c8d9b51ec8
Odbayar23 <odbayar.boldbaatar@smail.th-koeln.de>
2023-03-15 22:55:11 +0100

    Update in inverter part on GUI
    Efficiency values are updated,
    Information button data has been added

--- BifacialSimu_src/GUI.py
+++ BifacialSimu_src/GUI.py
@@ -177,13 +177,13 @@ inverterDict = {
     'inv_EuroEfficiency': 0, #European efficiency of inverter [%]
     'inv_CECEfficiency': 0, #California efficiency of inverter [%]
     'inv_WeightedEff': True, #Check whether weighted efficiency selector or not
-    'inv_Input1': 0, #Range for inverter input
-    'inv_Input2': 0.2, #Range for inverter input
-    'inv_Input3': 0.4, #Range for inverter input
-    'inv_Input4': 0.5, #Range for inverter input
-    'inv_Input5': 0.6, #Range for inverter input
-    'inv_Input6': 0.8, #Range for inverter input
-    'inv_Input7': 1, #Range for inverter input
+    'inv_Input1': 0, #Ranges for inverter efficinecy input
+    'inv_Input2': 0.2, #Ranges for inverter efficinecy input
+    'inv_Input3': 0.4, #Ranges for inverter efficinecy input
+    'inv_Input4': 0.5, #Ranges for inverter efficinecy input
+    'inv_Input5': 0.6, #Ranges for inverter efficinecy input
+    'inv_Input6': 0.8, #Ranges for inverter efficinecy input
+    'inv_Input7': 1, #Ranges for inverter efficinecy input
     'inv_Effvalue1': 0, #Efficiency value for respective range
     'inv_Effvalue2': 0.96, #Efficiency value for respective range
     'inv_Effvalue3': 0.97, #Efficiency value for respective range
@@ -357,6 +357,39 @@ class Window(tk.Tk):
 
             more_info_button = Button(info_frame,text='More Information', command= lambda:github_infopage("yes"))
             more_info_button.grid(row=0,column=0)
+            
+            
+        
+        # Inverter Parameter tab
+        def button_IP():
+            # f= open(rootPath+"\Lib\Info_Messages\Inverter_Parameters.txt")
+            # text_IP= f.read()
+            # f.close()
+            # response = messagebox.askokcancel("Functions Info!", text_SP) 
+            # if response == 1:
+            #     webbrowser.open("https://github.com/cire-thk/BifacialSimu#readme",new=1)
+            # global pop_IP
+            pop_SP = Toplevel()
+            pop_SP.title("Inverter Parameter Info!")
+            pop_SP.geometry("1400x700")
+            pop_SP.iconbitmap(rootPath+"\Lib\Button_Images\info_logo.ico")
+            f= open(rootPath+"\Lib\Info_Messages\Inverter_Parameters.txt")
+            text_SP= f.read()
+            f.close()
+            # since ° is a unicode character it should be replaced by its character unicode to be read properly from .txt files
+            text_SP=text_SP.replace("Â°", "\u00b0")
+             
+            SP_Info = Label(pop_SP, text=text_SP,bg="white", font=("Arial",8),justify="left")
+            SP_Info.pack(pady=5)
+            
+            info_frame= Frame(pop_SP, bg="white")
+            info_frame.pack(pady=5)
+            
+
+            more_info_button = Button(info_frame,text='More Information', command= lambda:github_infopage("yes"))
+            more_info_button.grid(row=0,column=0)
+            
+            
 # =============================================================================
 #         assigning the info Button Icon to a variable
 # =============================================================================
@@ -379,6 +412,9 @@ class Window(tk.Tk):
         # Simulation Parameter tab
         Info_SP = Button(simulationParameter_frame, image=Info_image,command = button_SP, borderwidth=0)
         Info_SP.grid(row=0,column=1)
+        # Inverter Parameter tab
+        Info_IP = Button(inverterParameter_frame, image=Info_image,command = button_IP, borderwidth=0)
+        Info_IP.grid(row=0,column=2)
         
         
         namecontrol_frame.bind("<Configure>", self._on_frame_configure)
@@ -2264,7 +2300,7 @@ class Window(tk.Tk):
     
         
     
-    #Input power
+    #Inverter efficiency ranges
         Label_inv_Ranges=ttk.Label(inverterParameter_frame, text="Efficiency ranges:")
         Label_inv_Ranges.grid(row=9, column=1,padx=20, sticky="w")
         Label_inv_Input1=ttk.Label(inverterParameter_frame, text="Input 1:")
@@ -2298,7 +2334,7 @@ class Window(tk.Tk):
 
         
 
-    #Input power
+    #Efficincy values
         Label_inv_Effvalues=ttk.Label(inverterParameter_frame, text="Efficiency values:")
         Label_inv_Effvalues.grid(row=9, column=3,padx=20, sticky="w")
         Label_inv_Effvalue1=ttk.Label(inverterParameter_frame, text="Value 1:")
@@ -2940,8 +2976,7 @@ class Window(tk.Tk):
             
         canvas = FigureCanvasTkAgg(fig5, master=tk.Toplevel())
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1.0)
-        canvas.draw()
-
+        canvas.draw()        
 
 def gui():    
     root = Window()
